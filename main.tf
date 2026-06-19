b594712f15b3afa7393b7d0e70b9d3ba38390000
parasbedi13-star <parasbedi13@gmail.com>
2026-06-19 00:43:11 -0400

    Resource group code
    Resource group code

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+requried_providers {
+azurerm = {
+source = "hashicorp/azurerm"
+version = "4.77.0"
+}
+}
+}
+
+provider "azurerm" {
+features {}
+}
+
+
+Variable = "rg_details"
+
+resource "azurerm_resource_group" "rgs" {
+
+for_each = var.rg_details
+
+name = each.value.name
+location = each.value.location
+managed_by = each.value.managed_by
+
+}
+
+rg_details = {
+RG-1 = {name = "Zain01" , location = "Centralindia" , managed_by = "Bedi"}
+RG-2 = {name = "Zain02" , location = "westus" , managed_by = "devraj"}
+RG-3 = {name = "Zain03" , location = "eastus" , managed_by = "satinder"}
+}
